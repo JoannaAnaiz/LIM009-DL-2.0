84a47f6a3b2c94d5b4b6448603ae2a9b3ff38fc8
JoannaAnaiz <joanna.yucra@gmail.com>
2019-04-17 10:09:43 -0500

    Mostrando datos de los actores de Harry Potter

--- src/data.js
+++ src/data.js
@@ -1,10 +1,18 @@
 /* Manejo de data */
 
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+esta es una función de ejemplo
+puedes ver como agregamos la función a nuestro objeto global window
 
 const example = () => {
   return 'example';
 };
+
+
+
+
+
+
+
+
 
 window.example = example;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
 /* Manejo del DOM */
+
+const boxElement = document.getElementById('box');
+
+function obtener(){
+    fetch('data/potter.json')
+    .then( res => res.json())
+    .then(data => verData(data))
+   
+    const verData = (data) => {
+      let string = '';
+      for (let i = 0; i < data.length; i++) {
+        string += `<div class="card">
+                    <div>
+                      <img src=${data[i].image} alt=${data[i].name}/>
+                      <h2> Name actor: ${data[i].actor}</h2>
+                      <h3> Name character: ${data[i].name} </h3>
+                      <h4> House: ${data[i].house}</h4>
+                      <h4> Species: ${data[i].species}</p> 
+                                         
+                    </div> 
+                  </div>`
+       };
+      
+    return boxElement.innerHTML = string;
+  } 
+}
+obtener();
